020c0ca39354696a00a0f9efc5155ce0c1b67285
Kiran Anand <liteninkiran@hotmail.com>
2026-08-05 13:07:59 +0100

    028 - Adding Computed signals

--- 02 - ngrx-quiz/src/app/app.component.ts
+++ 02 - ngrx-quiz/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { SharedModule } from './shared.module';
 import { QuizStore } from './store/quiz.store';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { QuestionPresenterComponent } from './components/question-presenter/question-presenter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [SharedModule],
+  imports: [SharedModule, ToolbarComponent, QuestionPresenterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

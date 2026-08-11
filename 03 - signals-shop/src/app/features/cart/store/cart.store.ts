d9caa3bd950d56a4708eef171a92243c98e4f819
Kiran Anand <liteninkiran@hotmail.com>
2026-08-11 20:35:24 +0100

    070 - Refactoring the missing methods

--- 03 - signals-shop/src/app/features/cart/store/cart.store.ts
+++ 03 - signals-shop/src/app/features/cart/store/cart.store.ts
@@ -1,4 +1,4 @@
-import { signalStore, withComputed, withProps, withState } from '@ngrx/signals';
+import { signalStore, withComputed, withMethods, withProps, withState } from '@ngrx/signals';
 import { initialCartSlice } from './cart.slice';
 import { computed, inject } from '@angular/core';
 import { ShopStore } from '../../../store/shop.store';
@@ -7,6 +7,11 @@ import { buildCartVm } from './cart.vm-builder';
 export const CartStore = signalStore(
   withState(initialCartSlice),
   withProps((_) => ({ _shopStore: inject(ShopStore) })),
+  withMethods((store) => ({
+    incrementQuantity: store._shopStore.incrementQuantity,
+    decrementQuantity: store._shopStore.decrementQuantity,
+    checkoutCart: store._shopStore.checkoutCart,
+  })),
   withComputed((store) => ({
     vm: computed(() =>
       buildCartVm(

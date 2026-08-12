71b753f58e69be9c7371ace58d1f4b82522f7e8a
Kiran Anand <liteninkiran@hotmail.com>
2026-08-12 18:23:43 +0100

    075 - The Pros and Cons of async methods

--- 02 - ngrx-quiz/src/app/services/dictionaries.service.ts
+++ 02 - ngrx-quiz/src/app/services/dictionaries.service.ts
@@ -16,13 +16,13 @@ export class DictionariesService {
   getDictionaryWithDelay(language: string): Observable<Dictionary> {
     const delay = Math.floor(Math.random() * 3000) + 1000;
     return of(this.dictionaryOf(language)).pipe(
-      tap((_) =>
-        console.log(
-          `Started loading for ${language.toUpperCase()} with ${(delay / 1000).toFixed(1)}s delay`,
-        ),
-      ),
+      // tap((_) =>
+      //   console.log(
+      //     `Started loading for ${language.toUpperCase()} with ${(delay / 1000).toFixed(1)}s delay`,
+      //   ),
+      // ),
       delayWhen(() => timer(delay)),
-      tap((_) => console.log('Finished')),
+      // tap((_) => console.log('Finished')),
     );
   }
 }

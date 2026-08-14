37d873cc9ae9180fe684196fc2555dc93bfcabab
Kiran Anand <liteninkiran@hotmail.com>
2026-08-14 19:30:27 +0100

    091 - The withEntities custom feature

--- 04 - books/src/app/pages/book-presenter/store/book-presenter.store.ts
+++ 04 - books/src/app/pages/book-presenter/store/book-presenter.store.ts
@@ -3,6 +3,7 @@ import {
   signalMethod,
   signalStore,
   withComputed,
+  withHooks,
   withMethods,
   withState,
 } from '@ngrx/signals';
@@ -10,14 +11,22 @@ import { initialBookPresenterSlice } from './book-presenter.slice';
 import { computed } from '@angular/core';
 import { BOOKS_COLLECTION } from '../../../data/books-collection';
 import { withDevtools } from '@angular-architects/ngrx-toolkit';
+import { setAllEntities, withEntities } from '@ngrx/signals/entities';
+import { Book } from '../../../models/book.model';
 
 export const BookPresenterStore = signalStore(
   withState(initialBookPresenterSlice),
+  withEntities<Book>(),
   withComputed((store) => ({
-    book: computed(() => BOOKS_COLLECTION[store.id()]),
+    book: computed(() => store.entityMap()[store.id()]),
   })),
   withMethods((store) => ({
     setBookId: signalMethod<number>((id) => patchState(store, { id })),
   })),
   withDevtools('book-presenter'),
+  withHooks((store) => ({
+    onInit: () => {
+      patchState(store, setAllEntities(BOOKS_COLLECTION));
+    },
+  })),
 );
